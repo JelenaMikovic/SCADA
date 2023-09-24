40e69ba21660fa6e05205f6b4faae17353424f26
Jelena Mikovic <jelena.mikovic189@gmail.com>
2023-09-24 20:08:02 +0200

    Added CRUD for all tags
    Closes #8
    Closes #9
    Closes #10

--- scada_front/scada_front/src/app/services/tag.service.ts
+++ scada_front/scada_front/src/app/services/tag.service.ts
@@ -9,6 +9,7 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class TagService {
 
+
   constructor(private http: HttpClient) { }
   private baseUrl = 'http://localhost:5184/api/Tag';
 
@@ -16,7 +17,18 @@ export class TagService {
     return this.http.get<any>(`${this.baseUrl}`);
   }
 
+  deleteTag(id: number) : Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/` + id);
+  }
 
+  editTag(dto: TagDTO): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}`, dto);
+  }
+
+  createTag(dto: { name: string; ioAddress: string; description: string; value: number; lowLimit?: number; highLimit?: number; scanTime: number; isScanOn: boolean; type: string; unit?: string; }): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}`, dto);
+  }
+  
 }
 
 
@@ -26,7 +38,7 @@ export interface TagDTO {
   id: number;
   name:string;
   description: string;
-  iOAddress:string;
+  ioAddress:string;
   value: number;
   lowLimit?: number;
   highLimit?: number;
